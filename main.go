c5e8171aa30930ece060bc85b675f85c0486b946
Danil <danil.volf.2000@mail.ru>
2026-01-16 20:17:58 +0300

    second practik

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+    "encoding/json"
+    "fmt"
+    "os"
+)
+
+type Movie struct {
+    Title string `json:"title"`
+    Genre string `json:"genre"`
+}
 
 func main() {
-    fmt.Println("Hello from Git practice!")
+    data, err := os.ReadFile("movies.json")
+    if err != nil {
+        panic(err)
+    }
+
+    var movies []Movie
+    json.Unmarshal(data, &movies)
+
+    for _, m := range movies {
+        fmt.Printf("🎬 %s (%s)\n", m.Title, m.Genre)
+    }
 }
